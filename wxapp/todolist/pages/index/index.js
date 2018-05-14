2f656de2f72c742400ec808172d7510002245973
tanglin <2196184435@qq.com>
2018-05-14 22:38:11 +0800

    ToDoList

--- wxapp/todolist/pages/index/index.js
+++ wxapp/todolist/pages/index/index.js
@@ -1,5 +1,6 @@
 //index.js
 //获取应用实例
+const util = require('../../utils/util.js')
 const app = getApp()
 
 Page({
@@ -13,100 +14,268 @@ Page({
 		// 界面状态要被 data 全面接管
 		addFormShow: false,
 		addText: '',
-		lists: [],
+		lists: []
 	},
-	
+
 	curLists: [],
 
+	reset: function(lists) {
+		lists = lists == '' ? [] : lists
+		lists.forEach(item => {
+			item.contStyle = ''
+		})
+	},
+
 	//事件处理函数
-
-	showLists(e) {
-		// e 事件对象
-		const status = e.currentTarget.dataset.status
-
+	// 删除
+	deleteItem: function (e) {
+		let that = this
+		const index = e.currentTarget.dataset.id
 		const temp = this.data.lists
-	
+		wx.showModal({
+			title: '您确定要删除吗？',
+			content: '',
+			cancelText: '考虑一下',		// 最大四个汉字
+			success: function (res) {
+				if (res.confirm) {
+					console.log('删除成功')
+					temp.splice(index, 1)
+					that.setData({
+						lists: temp,
+						curLists: temp
+					})
+					that.save()
+				} else {
+					console.log('取消')
+				}
+
+			}
+		})
+	},
+
+	// 触摸开始
+	touchS: function (e) {
+		// console.log('开始：' + JSON.stringify(e))
+		// 是否只有一个触摸点
+		if (e.touches.length === 1) {
+			// console.log(e.touches[0].clientX)
+			// return
+			this.setData({
+				// 触摸起始的X坐标
+				startX: e.touches[0].clientX
+			})
+		}
+	},
+	// 触摸移动
+	touchM: function (e) {
+		// console.log(e)
+		// console.log('移动：' + JSON.stringify(e))
+		let that = this
+		if (e.touches.length === 1) {
+			// 触摸点的X坐标
+			let moveX = e.touches[0].clientX
+			// console.log(moveX)
+			// 计算手指起始点的X坐标与当前触摸点的X坐标的差值
+			let disX = that.data.startX - moveX
+
+			// console.log(disX)
+			// return
+
+			let contStyle = ''
+			if (disX < 0) {
+				contStyle = 'moveRight'
+			} else if (disX > 0) {
+				contStyle = 'moveLeft'
+			}
+			// 获取手指触摸的是哪一个item
+			const index = e.currentTarget.dataset.id
+			let temp = that.data.lists
+
+			// 将拼接好的样式设置到当前item中
+			temp[index].contStyle = contStyle
+			// 更新列表的状态
+			this.setData({
+				curLists: temp,
+				status: 1
+			});
+		}
+	},
+
+	//保存方法，将todo-list和todo-logs保存在小程序本地，通过调用小程序开放api wx.setStorageSync()
+	save: function () {
+		wx.setStorageSync('todo_list', this.data.lists)
+		// wx.setStorageSync('todo_logs', this.data.logs)
+	},
+
+	//加载本地缓存中的todo-list
+
+	load: function () {
+		const lists = wx.getStorageSync('todo_list')
+
+		this.reset(lists)
+		this.setData({
+			lists: lists,
+			curLists: lists
+		})
+	},
+
+	onLoad: function () {
+		this.load()
+		// wx.clearStorage('todo_list')
+	},
+
+	showStatus: function (status, target) {
+		this.reset(target)
+		// console.log(target)
 		if (status == '1') {
-			this.curLists = [...temp]
+			return [...target]
 		}
 		// 未完成
 		if (status == '2') {
-			this.curLists = temp.map(item => {
+			return target.map(item => {
 				return item.status == '0' ? item : ''
 			})
 			console.log(this.curLists)
 		}
 		// 已完成
 		if (status == '3') {
-			this.curLists = temp.map(item => {
+			return target.map(item => {
 				return item.status == '1' ? item : ''
 			})
 			console.log(this.curLists)
 		}
-		
+	},
+
+	showTime: function (time) {
+		if (time < 100) {
+			return '1分钟前'
+		} else if (100 < time && time < 6000) {
+			return Math.floor(time / 100) + '分钟前'
+		} else if (6000 < time & time < 1000000) {
+			return Math.floor(time / 10000) == 0 ? 1 + '小时前' : Math.floor(time / 10000) + '小时前'
+		} else {
+			return util.formatTime(new Date())
+		}
+	},
+
+	showLists(e) {
+		// e 事件对象
+		const status = e.currentTarget.dataset.status
+
+		const temp = this.data.lists == '' ? [] : this.data.lists
+		// console.log(temp)		
+
+		this.curLists = this.showStatus(status, temp)
+
 		for (let i = 0; i < this.curLists.length + 1; i++) {
 			this.curLists.forEach((item, index) => {
 				if (item == '') {
-					console.log(item, index)
+					// console.log(item, index)
 					this.curLists.splice(index, 1)
 				}
 			})
 		}
 
+		// 更新时间
+		const reg = /\/|\s+|:/g
+
+		// console.log(util.formatTime(new Date()))
+		// return;
+		temp.forEach(item => {
+			// console.log(item.time)
+			let curTime = util.formatTime(new Date()).replace(reg, '')
+			let date = Number(curTime - item.time.replace(reg, ''))
+
+			item.date = this.showTime(date)
+		})
+
+
 		this.setData({
 			status: status,
 			curLists: this.curLists
 		})
+
+	},
+
+	addTodo: function (e) {
 		
-	},
-	addTodoShow: function (e) {
-		this.setData({
-			addFormShow: true
-		})
-	},
-	addTodo: function(e) {
+		if (!this.data.addText || !this.data.addText.trim()) {
+			wx.showToast({
+				title: '请输入',
+				icon: 'loading',
+				duration: 1000
+			})
+			return
+		}
 		// 如何添加一个新的 ToDo
 		// 页面上多一条任务
 		// 显示多少条 由 lists 决定
 		// lists push
 		// 数据驱动界面 数据改变界面自动更新
 
+		// 取当前时间
+		// const reg = /\/|\s+|:/g
+		// const time = util.formatTime(new Date()).replace(reg, '')
+		const time = util.formatTime(new Date())
+
 		// 获取输入框的内容
 		const task = {
 			title: this.data.addText,
 			// title: '烤面筋可带劲了',
 			date: '刚刚',
-			status: '0'
+			time: time,
+			status: '0',
+			contStyle: '',
+			id: time
 		}
 
 		// es6 数组插入数据
 		const temp = [...this.data.lists, task]
-		console.log(temp)
+		// console.log(temp)
+
 		this.setData({
 			lists: temp,
 			curLists: temp,
 			addFormShow: false
 		})
 
+		this.save()
+
 		// 看到界面，我们就知道要的数据
 		// 看到交互，了解对数据的操作
 	},
-	addTodoHide: function(e) {
+
+	addTodoShow: function (e) {
+		this.load()
+		this.setData({
+			addFormShow: true
+		})
+		this.save()
+	},
+
+	addTodoHide: function (e) {
 		this.setData({
 			addFormShow: false
 		})
+		this.save()
 	},
-	setInput: function(e) {
+
+	setInput: function (e) {
 		// console.log(e.detail.value)
 		this.setData({
 			addText: e.detail.value
 		})
 	},
-	changeTodo: function(e) {
-		const index = e.currentTarget.dataset.item
+
+	changeTodo: function (e) {
+		if (this.data.status != 1) return
+		console.log(e)
+		const index = e.currentTarget.dataset.id
+		console.log(index)
 		const temp = this.data.lists
 		temp.forEach((item, i) => {
 			// console.log(item, i)
+			// return
 			if (i == index) {
 				if (item.status == '0') {
 					item.status = '1'
@@ -120,7 +289,8 @@ Page({
 					item.status = '0'
 					wx.showToast({
 						title: '任务打回重做',
-						icon: 'circle',
+						// icon只支持 success loading
+						// icon: 'circle',
 						duration: 1000
 					})
 				}
@@ -128,11 +298,14 @@ Page({
 		})
 		this.setData({
 			lists: temp,
-			curLists: temp
+			curLists: temp,
+			status: 1
 		})
 		// 当前点击条目的 status => success
 		// 数据 lists 跟当前条目相关的这一条数据
 		// status = 1
 		// index
+
+		this.save()
 	}
 })
--- wxapp/todolist/readme.md
+++ wxapp/todolist/readme.md
@@ -27,3 +27,73 @@
     wx:key="index"          和 for 同时出现，   
     {{item.id}}             获取动态数据，默认每一项为一个 item(item.title、item.id。。。)
 ```
+
+- 弹出层表单 移动端屏幕小 层的概念 z-index
+```css
+display: none;
+position: absolute;
+```
+
+
+- 消息提示框  
+```  js
+wx.showToast({
+    title: '请输入',
+    icon: 'loading',    // icon 只支持 success 、loading
+    duration: 1000
+})
+```
+```js
+wx.showModal({
+    title: '提示',
+    content: '模态弹窗',
+    success: function (res) {
+        if (res.confirm) {
+            console.log('用户点击确定')
+        } else {
+            console.log('用户点击取消')
+        }
+    }
+})
+```
+```js
+wx.showActionSheet({
+    itemList: ['A', 'B', 'C'],
+    success: function(res) {
+        console.log(res.tapIndex)
+    },
+    fail: function(res) {
+        console.log(res.errMsg)
+    }
+})
+```
+
+- 数据缓存
+```js
+wx.setStorage(OBJECT) 
+wx.setStorageSync(KEY,DATA) //同步缓存
+// 获取
+wx.getStorage(OBJECT) 
+// 移除
+wx.removeStorage(OBJECT) 
+// 清空
+wx.clearStorage() 
+```
+```js
+    // 本地保存lists 
+    wx.setStorageSync('todo_list', this.data.lists)
+    const lists = wx.getStorageSync('todo_list')
+```
+
+- 获取系统时间日期
+```js
+    //在小程序中，新建项目时，就会有一个utils.js文件，就是获取日期和时间的    
+    const util = require('../../utils/util.js'); 
+    const time = util.formatTime(new Date());  
+    // 格式为： 2018/05/14 13:59:19
+```
+
+- 触摸事件  
+touchstart  
+touchmove   
+touchend    
