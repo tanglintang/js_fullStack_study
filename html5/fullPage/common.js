8dded51e583210d0c273fd44c62b46cf7faf4d74
tanglin <2196184435@qq.com>
2018-06-13 22:22:43 +0800

    全屏滚动插件-截流&防抖

--- html5/fullPage/common.js
+++ html5/fullPage/common.js
@@ -1,11 +1,27 @@
 class PureFullPage {
     // constructor 属性定义
     constructor(options) {
+        // 默认配置项
+        const defaultOptions = {
+            // 垂直方向上的滚动条
+            isShowNav: true,
+            delay: 1000,
+            // 每次turnpage的callback
+            definePages: () => {}
+        }
+        // Object.assign
+        // 如果目标对象中的属性具有相同的键，则属性将被源中的属性覆盖。后来的源的属性将类似地覆盖早先的属性。
+        this.options = Object.assign(defaultOptions, options)
+        console.log(options)
+
         this.container = document.getElementById('pureFullPage')
         // this.page = this.container.get
         this.clientHight = document.documentElement.clientHeight
         // 手动计算 page 高度
         this.pageHeight = document.querySelector('#pureFullPage').offsetHeight
+        // 提供默认配置项
+        this.DELAY = this.options.delay
+        this.currentPosition = 0
         this.init()
     }
     init() {
@@ -20,16 +36,59 @@ class PureFullPage {
         // this => 函数 的作用域
         // delay => 推迟执行
 
-        const handleMouseWheel = utils.throttle(this.scrollMouse, this, 1000)
+        const handleMouseWheel = utils.throttle(this.scrollMouse, this, this.DELAY)
 
         if ( navigator.userAgent.toLowerCase().indexOf('firefox') == -1 ) {
             document.addEventListener('mousewheel', handleMouseWheel, false)
         } else {
             document.addEventListener('DOMMouseScroll', handleMouseWheel, false)
         }
+
+        // 事件处理函数交由对象的方法执行
+        window.addEventListener('resize', this.handleWindowResize.bind(this), false)
+
     }
     // 滚轮事件处理函数
-    scrollMouse(e) {
-        console.log(e)
+    scrollMouse(event) {
+        // 偏移量
+        // 兼容问题，单独处理
+        console.log(event)
+        const delta = utils.getWheelDelta(event)
+        if (delta > 0) {
+            this.goUp()
+        } else {
+            this.goDown()
+        }
+    }
+
+    handleWindowResize(event) {
+        // 作为对象的方法，肯定有需要找到那个对象
+        // this 代表那个对象
+        // 防抖
+        utils.debounce(this.getNewPosition, this, event, this.DELAY)
+    }
+    getNewPosition() {
+        console.log('debounce')
+        this.viewHeight = document.documentElement.clientHeight
+        this.container.style.height = this.viewHeight + 'px'
+        console.log(document.documentElement.clientHeight)
+    }
+
+    goUp() {
+        this.currentPosition += this.clientHight
+        
+        if (this.currentPosition > 0 ) {
+            this.currentPosition = 0
+        }
+        this.container.style.top = `${this.currentPosition}px`
+    }
+    goDown() {
+        this.currentPosition -= this.clientHight
+        
+        if (this.currentPosition < -2*this.clientHight ) {
+            this.currentPosition = -2*this.clientHight
+        }
+        this.container.style.top = `${this.currentPosition}px`
+        
     }
 }
--- html5/fullPage/utils.js
+++ html5/fullPage/utils.js
@@ -1,13 +1,22 @@
+// 截流 throttle，
+// 在规定时间内只执行一次，先执行
+// 防抖 debounce 
+// 在规定时间后执行一次，后执行
+
+// 都为解决高频事件
+// scroll mousewheel mouseover mouesmove touchmove onresize
+
 const utils = {
     // method 规定时间内执行的次数只有一次
-    // method 执行时 函数内的 this 指向 pureFullPage
+    // method 执行时 函数内的 this 指向 pureFullPage 不然就是指向全局
     // context 上下文环境
     throttle(method, context, delay) {
         // 返回时的函数就是事件执行的真正的函数本体
         // args 事件对象 event
         // 执行时上下文环境要和以前一样
         let wait = false
-        return function(...args) {
+        // 闭包
+        return function (...args) {
             console.log(wait)
             if (!wait) {
                 method.apply(context, args)
@@ -17,5 +26,23 @@ const utils = {
                 }, delay)
             }
         }
+    },
+    debounce(method, context, event, delay) {
+        // window.resize
+        // setTimeout 多少秒后执行，只执行一次
+        // setInterval() 隔多少秒执行一次，不停止就一直执行
+        clearTimeout(context.timer)
+        // 保持定时器？使用变量会在每次执行后销毁，给 context 函数添加一个方法可以一直保存
+        context.timer = setTimeout(() => {
+            method.call(context, event)
+        }, delay)
+    },
+    getWheelDelta(event) {
+        // console.log(event);
+        if (event.wheelDelta) {
+            this.getWheelDelta = event => event.wheelDelta;
+            return event.wheelDelta;
+        }
     }
+
 }
